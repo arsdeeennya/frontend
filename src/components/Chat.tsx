0b33ccf236717ea6983b89576cbcb9e67317c9de
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-07-11 23:24:59 +0900

    文言変更

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,7 +5,7 @@ export const Chat = () => {
   return (
 
     <h1 style={{textAlign: 'center', color: '#3f51b5'}}>
-      <ForumIcon/> なし。 <ForumIcon/> 
+      <ForumIcon/> なし。,, <ForumIcon/> 
     </h1>
   )
 }
